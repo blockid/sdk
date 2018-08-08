521bc5e2adbebdc9dc6b860b0e6b1578c8de84eb
Stanisław Głogowski <stanislaw.glogowski@gmail.com>
2018-08-08 06:41:45 +0200

    Updates hex to buffer

--- src/utils/eth.ts
+++ src/utils/eth.ts
@@ -1,22 +1,21 @@
-import * as createSha3 from "keccak";
 import { recover } from "secp256k1";
 import { publicKeyToAddress } from "./ecdsa";
-import { anyToHex } from "./hex";
+import { anyToBuffer } from "./buffer";
+import { sha3 } from "./crypto";
 
 export function hashPersonalMessage(message: Buffer | string): Buffer {
-  const hex = anyToHex(message);
-  const prefix = anyToHex("\x19Ethereum Signed Message:\n" + (hex.length / 2).toString(10));
-
-  return createSha3("keccak256")
-    .update(Buffer.from(`${prefix}${hex}`, "hex"))
-    .digest();
+  const messageBuff = anyToBuffer(message);
+  return sha3(
+    anyToBuffer("\x19Ethereum Signed Message:\n" + messageBuff.length),
+    messageBuff,
+  );
 }
 
 export function personalRecoverPublicKey(message: Buffer | string, signed: Buffer | string): Buffer {
   const messageHash = hashPersonalMessage(message);
-  const signedHex = anyToHex(signed);
-  const signature = Buffer.from(signedHex.slice(0, -2), "hex");
-  const recovery = parseInt(signedHex.slice(-2), 16) - 27;
+  const signedBuff = anyToBuffer(signed);
+  const signature = signedBuff.slice(0, -1);
+  const recovery = signedBuff[ signedBuff.length - 1 ] - 27;
 
   return recover(
     messageHash,
