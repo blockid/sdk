0a098717b056a0b3108221c261e070ff857ab1a5
Stanisław Głogowski <stanislaw.glogowski@gmail.com>
2018-08-10 11:00:03 +0200

    Updates tests

--- src/utils/ecdsa.spec.ts
+++ src/utils/ecdsa.spec.ts
@@ -5,30 +5,32 @@ const PRIVATE_KEY = Buffer.from("571c876e806cb42ac54f6dd7bf9c73c229543a3de0d43bc
 const PUBLIC_KEY = publicKeyCreate(PRIVATE_KEY, false);
 const ADDRESS = "0x194eb1c67b25cc851d6037641fcd9f0b65ae7d90";
 
-describe("utils/ecdsa", () => {
+describe("utils", () => {
+  describe("ecdsa", () => {
 
-  describe("generatePrivateKey()", () => {
+    describe("generatePrivateKey()", () => {
 
-    it("should generates private key", () => {
-      const privateKey = generatePrivateKey();
-      expect(Buffer.isBuffer(privateKey)).toBeTruthy();
-      expect(privateKey.length).toBe(32);
+      it("should generates private key", () => {
+        const privateKey = generatePrivateKey();
+        expect(Buffer.isBuffer(privateKey)).toBeTruthy();
+        expect(privateKey.length).toBe(32);
+      });
     });
-  });
 
-  describe("privateKeyToAddress()", () => {
+    describe("privateKeyToAddress()", () => {
 
-    it("should converts private key to address", () => {
-      const address = privateKeyToAddress(PRIVATE_KEY);
-      expect(address).toBe(ADDRESS);
+      it("should converts private key to address", () => {
+        const address = privateKeyToAddress(PRIVATE_KEY);
+        expect(address).toBe(ADDRESS);
+      });
     });
-  });
 
-  describe("publicKeyToAddress()", () => {
+    describe("publicKeyToAddress()", () => {
 
-    it("should converts public key to address", () => {
-      const address = publicKeyToAddress(PUBLIC_KEY);
-      expect(address).toBe(ADDRESS);
+      it("should converts public key to address", () => {
+        const address = publicKeyToAddress(PUBLIC_KEY);
+        expect(address).toBe(ADDRESS);
+      });
     });
   });
 });
