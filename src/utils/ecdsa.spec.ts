36ecb57355358f72a1f6c8e8a7ff0c94a5047f3a
Stanisław Głogowski <stanislaw.glogowski@gmail.com>
2018-08-10 11:36:42 +0200

    Updates tests

--- src/utils/ecdsa.spec.ts
+++ src/utils/ecdsa.spec.ts
@@ -1,12 +1,12 @@
 import { generatePrivateKey, privateKeyToAddress, publicKeyToAddress } from "./ecdsa";
 import { publicKeyCreate } from "secp256k1";
 
-const PRIVATE_KEY = Buffer.from("571c876e806cb42ac54f6dd7bf9c73c229543a3de0d43bc02d6ead41a76aae0c", "hex");
-const PUBLIC_KEY = publicKeyCreate(PRIVATE_KEY, false);
-const ADDRESS = "0x194eb1c67b25cc851d6037641fcd9f0b65ae7d90";
-
 describe("utils", () => {
   describe("ecdsa", () => {
+
+    const PRIVATE_KEY = Buffer.from("571c876e806cb42ac54f6dd7bf9c73c229543a3de0d43bc02d6ead41a76aae0c", "hex");
+    const PUBLIC_KEY = publicKeyCreate(PRIVATE_KEY, false);
+    const ADDRESS = "0x194eb1c67b25cc851d6037641fcd9f0b65ae7d90";
 
     describe("generatePrivateKey()", () => {
 
